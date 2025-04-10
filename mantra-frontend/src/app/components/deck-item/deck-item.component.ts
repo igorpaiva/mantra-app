b8d54112c94c658879b0105f7ec45f2e28e7644b
Igor Paiva <igor.araujo.ee@gmail.com>
2025-04-10 13:08:41 +0200

    [Front] - Fix cards display

--- mantra-frontend/src/app/components/deck-item/deck-item.component.ts
+++ mantra-frontend/src/app/components/deck-item/deck-item.component.ts
@@ -1,9 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Deck } from '../../model/deck.type';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-deck-item',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './deck-item.component.html',
   styleUrl: './deck-item.component.css'
 })
